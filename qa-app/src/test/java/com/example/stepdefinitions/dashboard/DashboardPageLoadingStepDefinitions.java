a791028fb9c941f88a857df683d933be4f814781
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2026-01-28 10:16:51 +0530

    Add dashboard loading right after login tests

---
+++ qa-app/src/test/java/com/example/stepdefinitions/dashboard/DashboardPageLoadingStepDefinitions.java
@@ -0,0 +1,45 @@
+package com.example.stepdefinitions.dashboard;
+
+import static org.junit.Assert.assertTrue;
+
+import org.openqa.selenium.WebDriver;
+
+import com.example.pages.login.LoginPage;
+import com.example.stepdefinitions.Hooks;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DashboardPageLoadingStepDefinitions {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+        driver = Hooks.getDriver(); 
+        loginPage = new LoginPage(driver);
+        
+        loginPage.goToLoginPage();
+    }
+
+    @When("the user enters valid credentials")
+    public void theUserEntersValidCredentials() {
+        loginPage.entersValidCredentials();
+    }
+
+    @Then("clicks the login button")
+    public void clicksTheLoginButton() {
+        loginPage.clickLoginButton();
+    }
+
+    @Then("the dashboard should load immediately")
+    public void theUserShouldBeRedirectedToTheDashboard() {
+        assertTrue(
+                "Admin is not navigated to dashboard page",
+                loginPage.isOnDashboardPage()
+        );
+    }
+    
+}
