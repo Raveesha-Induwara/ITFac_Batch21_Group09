4a6de1236f457f6cd5ed1c3c5ab323efac6d3bf2
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2026-02-01 23:51:11 +0530

    Thread sleep time added to stop field unidentified error Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- qa-app/src/test/java/com/example/stepdefinitions/plants/PlantAddSuccessStepDefinition.java
+++ qa-app/src/test/java/com/example/stepdefinitions/plants/PlantAddSuccessStepDefinition.java
@@ -14,7 +14,8 @@ public class PlantAddSuccessStepDefinition {
         plantPage = new AddPlantPage(Hooks.getDriver());
     }
     @And("the new plant {string} should appear in the plant list")
-    public void verifyNewPlantInList(String plantName) {
+    public void verifyNewPlantInList(String plantName) throws InterruptedException {
+        Thread.sleep(200);
         boolean isFound = plantPage.isPlantNameVisibleInTable(plantName);
 
         // This will display the message if isFound is false
