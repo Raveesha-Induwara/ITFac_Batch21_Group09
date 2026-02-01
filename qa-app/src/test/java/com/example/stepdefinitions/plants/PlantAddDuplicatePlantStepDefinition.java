e252f8b53b0e040bfd4f7762c568ad8bd6d7fe2b
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2026-02-01 11:26:28 +0530

    duplicate check of plant add Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

---
+++ qa-app/src/test/java/com/example/stepdefinitions/plants/PlantAddDuplicatePlantStepDefinition.java
@@ -0,0 +1,24 @@
+package com.example.stepdefinitions.plants;
+
+import org.junit.Assert;
+
+import com.example.pages.plants.AddPlantPage;
+import com.example.stepdefinitions.Hooks;
+
+import io.cucumber.java.en.Then;
+
+
+public class PlantAddDuplicatePlantStepDefinition {
+
+    private AddPlantPage plantPage;
+
+    public PlantAddDuplicatePlantStepDefinition() {
+        plantPage = new AddPlantPage(Hooks.getDriver());
+    }
+
+    @Then("the system should show the message {string}")
+    public void verifyDuplicateSubmission(String expectedMessage) {
+        String actualMessage = plantPage.AlertMsg();
+        Assert.assertEquals("The alert message did not match!", expectedMessage, actualMessage);
+    }
+}
